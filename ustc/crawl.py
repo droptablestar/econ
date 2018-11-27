4bef037b922109f7dfdb7d74fb6676f6b4387a72
josh reese <droptablestar@gmail.com>
2018-11-27 17:38:07 -0600

    Threading for all crawl.

--- ustc/crawl.py
+++ ustc/crawl.py
@@ -9,11 +9,17 @@ from re import findall, search
 from random import uniform
 from time import sleep, localtime, strftime
 from threading import Thread
-# import sys
+import sys
 
 from mongo import DB
 from pymongo.errors import DuplicateKeyError
 
+import numpy
+
+from pathos.multiprocessing import ProcessingPool as Pool
+from itertools import repeat
+# from functools import partial
+print('there')
 class Crawler:
     def __init__(self, file_name=None, domain=None, classification=None):
         r''
@@ -21,60 +27,50 @@ class Crawler:
         self.domain = domain
 
         db = DB('ustc')
-        if classification:
-            self.classification = classification
-            # self.classification = parse.quote(classification(' ', '+'))
-            self.collection = db.get_collection(classification.replace('+', '_'))
-            if 'url_1' not in self.collection.index_information().keys():
-                print('here')
-                self.collection.create_index('url', unique=True)
+        self.classification = classification.replace('+', '_') if classification else 'unclassified'
+        self.collection = db.get_collection(self.classification)
+        if 'url_1' not in self.collection.index_information().keys():
+            # if we're here it means we already created the unique index
+            self.collection.create_index('url', unique=True)
+
+        if self.classification != 'unclassified':
+            self.url = 'https://ustc.ac.uk/index.php/search/cicero?tm_fulltext=&tm_field_allauthr=&tm_translator=&tm_editor=&ts_field_short_title=&tm_field_imprint=&tm_field_place=&sm_field_year=&f_sm_field_year=&t_sm_field_year=&sm_field_country=&sm_field_lang=&sm_field_format=&sm_field_digital=&sm_field_class=%22'+self.classification+'%22&tm_field_cit_name=&tm_field_cit_no=&order=&sm_field_ty=&start={}'
+        else:
+            self.url = 'https://ustc.ac.uk/index.php/search/cicero?tm_fulltext=&tm_field_allauthr=&tm_translator=&tm_editor=&ts_field_short_title=&tm_field_imprint=&tm_field_place=&sm_field_year=&f_sm_field_year=&t_sm_field_year=&sm_field_country=&sm_field_lang=&sm_field_format=&sm_field_digital=&sm_field_class=&tm_field_cit_name=&tm_field_cit_no=&order=&sm_field_ty=&start={}'
+
         # db.count_all()
         # exit()
 
     def crawl_all(self):
-        i = 115050
-        end = i+1
-        is_set = False
-        if self.classification:
-            url = 'https://ustc.ac.uk/index.php/search/cicero?tm_fulltext=&tm_field_allauthr=&tm_translator=&tm_editor=&ts_field_short_title=&tm_field_imprint=&tm_field_place=&sm_field_year=&f_sm_field_year=&t_sm_field_year=&sm_field_country=&sm_field_lang=&sm_field_format=&sm_field_digital=&sm_field_class=%22'+self.classification+'%22&tm_field_cit_name=&tm_field_cit_no=&order=&sm_field_ty=&start={}'
-        else:
-            url = 'https://ustc.ac.uk/index.php/search/cicero?tm_fulltext=&tm_field_allauthr=&tm_translator=&tm_editor=&ts_field_short_title=&tm_field_imprint=&tm_field_place=&sm_field_year=&f_sm_field_year=&t_sm_field_year=&sm_field_country=&sm_field_lang=&sm_field_format=&sm_field_digital=&sm_field_class=&tm_field_cit_name=&tm_field_cit_no=&order=&sm_field_ty=&start={}'
-        while i <= end:
-            print(url.format(i))
-            print('i={}, end={}'.format(i, end))
+        def crawl(name, page_nos):
+            for n in page_nos:
+                self.log('{}_all.log'.format(name), '{}:{}:{}'.format(name, self.collection.name, self.url.format(n)))
+                text = self.read_url(self.url.format(n))
 
-            while True:
-                try:
-                    response = request.urlopen(url.format(i))
-                    break
-                except HTTPError as e:
-                    print('here')
-                    self.log('./errors.log', e)
-                    self.req_sleep()
+                records = findall(r'/record/[0-9]+', text)
+                for r in records:
+                    try:
+                        self.collection.insert_one({'url': '{}{}'.format(self.domain, r)})
+                    except DuplicateKeyError as e:
+                        self.log('./db_errors.log', e)
 
-            text = response.read()
-            try:
-                text = text.decode('utf-8')
-            except Exception:
-                text = text.decode('latin1')
+                self.req_sleep()
+                return
 
-            if not is_set:
-                is_set = True
-                end = search(r'(Results found: )(\d+)', text)
-                end = int(end.group(2)) if end else 0
-                print('end:', end)
+        text = self.read_url(self.url.format(0))
+        end = search(r'(Results found: )(\d+)', text)
+        end = int(end.group(2)) + 1 if end else 0
 
-            records = findall(r'/record/[0-9]+', text)
-            for r in records:
-                try:
-                    self.collection.insert_one({'url': '{}{}'.format(self.domain, r)})
-                except DuplicateKeyError as e:
-                    self.log('./db_errors.log', e)
+        inc = search(r'(showing 1 to )(\d+)', text)
+        inc = int(inc.group(2)) if end else 0
 
-            i += 25
-            self.req_sleep()
+        thread_count = 6
+        splits = numpy.array_split(numpy.array(range(0, end, inc)), thread_count)
+        for (i, s) in zip(range(0, thread_count), splits):
+            Thread(target=crawl, kwargs={'name': 't{}'.format(i), 'page_nos': s}).start()
 
-    def crawl_single(self, name):
+    def crawl_single(self, name, page_nos):
+        return
         records = self.collection.find({'html': {'$exists': False}})
         for r in records:
             self.log('{}.log'.format(name), '{}:{}:{}'.format(name, self.collection.name, r['url']))
@@ -88,38 +84,45 @@ class Crawler:
                     self.req_sleep()
             self.req_sleep()
 
-    def log(self, log_name, error):
+    def log(self, log_name, msg):
         with open(log_name, 'a') as f:
-            f.write('{} {}\n'.format(strftime('%Y-%d-%m_%H:%M:%S', localtime()), error))
+            f.write('{} {}\n'.format(strftime('%Y-%d-%m_%H:%M:%S', localtime()), msg))
 
     def req_sleep(self):
         timer = uniform(.5, 1.0)
         # print('sleeping for: {}s'.format(timer))
         sleep(timer)
 
+    def read_url(self, url):
+        while True:
+            try:
+                response = request.urlopen(url)
+                break
+            except HTTPError as e:
+                self.log('./errors.log', e)
+                self.req_sleep()
+        text = response.read()
+        try:
+            text = text.decode('utf-8')
+        except Exception:
+            text = text.decode('latin1')
+        return text
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
 if __name__ == '__main__':
-    def parse(name, i, lines):
-        start = i
-        end = i + 1
-        for j in range(start, end):
-            c = Crawler(domain='https://ustc.ac.uk/index.php', classification=lines[j])
-            c.crawl_single(name)
-            # print('{}:{} - [{}]'.format(j, lines[j], i))
-            # if i == 5:  # 37 classifications so special case for last one
-            #     c = Crawler(domain='https://ustc.ac.uk/index.php', classification=lines[j+1])
-            #     c.crawl_single(name)
-            # print('{}:{} - [{}]'.format(j+1, lines[j+1], i))
+    if len(sys.argv) == 1:
+        c = Crawler(domain='https://ustc.ac.uk/index.php')
+        c.crawl_all()
 
-    # c = Crawler('record_urls.txt', 'https://ustc.ac.uk/index.php', sys.argv[1] if len(sys.argv) > 1 else None)
-    with open('classifications.txt') as f:
-        lines = list(map(lambda line: line.strip(' \n'), f.readlines()))
-        for i in range(0, 9):  # create some threads
-            # print("t = Thread(target=parse, kwargs={{'name': t+str({}), 'i': {}, 'lines': lines}})".format(i, i+28))
-            t = Thread(target=parse, kwargs={'name': 't'+str(i), 'i': i+28, 'lines': lines})
-            t.start()
-
-        # c = Crawler(domain='https://ustc.ac.uk/index.php', classification=lines[i])
-        # for line in lines[0:1]:
-        #     c = Crawler('record_urls.txt', 'https://ustc.ac.uk/index.php', line)
-            # c.crawl_all()
-            # c.crawl_single()
+    elif len(sys.argv) == 2 and sys.argv[1] == '-c' < 1:
+        with open('classifications.txt') as f:
+            lines = list(map(lambda line: line.strip(' \n'), f.readlines()))
+            for i in range(0, len(lines)):  # create some threads
+                c = Crawler(domain='https://ustc.ac.uk/index.php', classification=lines[i])
+                Thread(target=c.crawl_single, kwargs={'name': 't{}'.format(i)}).start()
+    else:
+        print('usage: python crawl.py -c')
