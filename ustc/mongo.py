905b95a60f3101418b93db7c40a4df58fbd10a43
josh reese <droptablestar@gmail.com>
2018-11-28 15:37:11 +0000

    Added some functionality to mongo.py

--- ustc/mongo.py
+++ ustc/mongo.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import pprint
 
 class DB:
     def __init__(self, db_name):
@@ -9,8 +10,12 @@ class DB:
     def get_collection(self, collection):
         return self.db[collection]
 
-    def count_all(self):
-        collections = self.db.collection_names()
+    def get_collections(self, exclude=None, system=False):
+        return filter(lambda c: c != exclude, self.db.collection_names())  if system \
+            else filter(lambda c: c != exclude, self.db.collection_names(include_system_collections=False))
+
+    def count_all(self, exclude=None):
+        collections = self.get_collections(exclude)
 
         print('Collections inside the db:')
         total = 0
@@ -19,3 +24,25 @@ class DB:
                 print(c, '-', self.db[c].count(), 'records')
                 total += self.db[c].count()
         print('Total: ', total)
+
+
+    # looking for a list of collection names and a single collection to compare 
+    # and the unique column name to check. e.g., find_differences([a,b], d, 'key') 
+    # will find all the records in collection d that don't exist in collections a and b
+    # and have the same value for the 'key' property
+    def find_differences(self, coll_list, coll, uid):
+        coll_list_uids = []
+        for c in coll_list:
+            pprint.pprint(c)
+            # coll_list_uids.extend(list(self.db[c].find({}, {'_id': 0, c: 1}, limit=10)))
+            docs = self.db[c].find({}, projection={uid: 1, '_id': 0})
+            coll_list_uids.extend([d[uid] for d in docs])
+        docs = self.db[coll].find({}, projection={uid: 1, '_id': 0})
+        coll_uids = ([d[uid] for d in docs])
+        print(len(coll_list_uids))
+        print(len(coll_uids))
+
+        coll_list_set = set(coll_list_uids)
+        print(len(coll_list_set))
+        diff = set(coll_uids) - coll_list_set
+        return diff
