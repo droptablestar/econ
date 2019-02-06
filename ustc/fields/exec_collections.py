855121c3866719a4db93a485d026c5e96d6cbf7f
josh reese <droptablestar@gmail.com>
2019-02-06 11:20:01 -0600

    Automated field collection.

--- ustc/fields/exec_collections.py
+++ ustc/fields/exec_collections.py
@@ -1,11 +1,10 @@
 import subprocess
 #  mongo -u econ -p crazyhorsemonument ustc --eval "var collection = 'Medical_Texts'" variety.js
-command = ['mongo ustc --eval "var collection = \'Medical_Texts\'" variety.js']
+command = 'mongo ustc --eval "var collection = \'Medical_Texts\'" variety.js > {}.txt'
 # command = ['mongo', '-u', 'econ', '-p', 'crazyhorsemonument', 'ustc', '--eval', '"var collection = \'Medical_Texts\'"', 'variety.js']
 with open('collections.csv') as fd:
     collections = fd.readline().split(',')
     for c in collections:
+        c = command.format('../'+c)
         print(c)
-        x = subprocess.run(command, shell=True, cwd='variety')
-        print('x:', x)
-        exit()
+        subprocess.run(c, shell=True, cwd='variety')
