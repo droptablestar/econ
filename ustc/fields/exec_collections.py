d05ba93321630062f74dfe58cb8875f2f10094cf
josh reese <droptablestar@gmail.com>
2019-02-07 21:33:30 +0000

    all fields csv

--- ustc/fields/exec_collections.py
+++ ustc/fields/exec_collections.py
@@ -1,10 +1,9 @@
 import subprocess
 #  mongo -u econ -p crazyhorsemonument ustc --eval "var collection = 'Medical_Texts'" variety.js
-command = 'mongo ustc --eval "var collection = \'Medical_Texts\'" variety.js > {}.txt'
-# command = ['mongo', '-u', 'econ', '-p', 'crazyhorsemonument', 'ustc', '--eval', '"var collection = \'Medical_Texts\'"', 'variety.js']
+# command = 'mongo ustc --eval "var collection = \'{}\'" variety.js > {}.txt'
+command = 'mongo -u econ -p crazyhorsemonument ustc --eval "var collection = \'{0}\'" variety.js > ../{0}.txt'
 with open('collections.csv') as fd:
     collections = fd.readline().split(',')
     for c in collections:
-        c = command.format('../'+c)
-        print(c)
+        c = command.format(c.strip(' \n'))
         subprocess.run(c, shell=True, cwd='variety')
--- ustc/fields/parse.py
+++ ustc/fields/parse.py
@@ -4,7 +4,9 @@ fields = set()
 for f in list(filter(lambda x: '.txt' in x, os.listdir())):
     print(f)
     with open(f) as fd:
-        lines = fd.readlines()[3:-1]
+        start = False
+        lines = fd.readlines()
+        lines = lines[25:-1] if len(lines) > 30 else lines[3:-1]
         for line in lines:
             print(line)
             fields.add(line.split('|')[1].strip())
