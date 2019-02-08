39069088d8d01924756cb82a87c719572be25ab2
josh reese <droptablestar@gmail.com>
2019-02-08 17:46:06 -0600

    Parser optimized

--- ustc/csvs/decode.py
+++ ustc/csvs/decode.py
@@ -2,12 +2,21 @@ import os
 import codecs
 import re
 
-for f in filter(lambda x: '.csv' in x, os.listdir('.')):
-    print(f)
-    with codecs.open(f, encoding='utf8') as fd:
-        c = fd.read()
-        c = c.encode('utf8').decode('unicode_escape')
-        for m in re.finditer('\"https?://[^\"]+\"', c):
-            c = c.replace(m.group(), m.group().replace(' ', '%20'))
-    with codecs.open(f, 'w', encoding='utf8') as fd:
-        fd.write(c)
+def quote():
+    for f in filter(lambda x: '.csv' in x, os.listdir('.')):
+        print(f)
+        with codecs.open(f, encoding='utf8') as fd:
+            c = fd.read().encode('utf8').decode('unicode_escape')
+            i = re.finditer('\"https?://[^\"]+\"', c)
+            prev = next(i)
+            new_str = c[:prev.start()] + get_url(c, prev)
+            for cur in i:
+                new_str += c[prev.end():cur.start()] + get_url(c, cur)
+                prev = cur
+        with codecs.open(f, 'w', encoding='utf8') as fd:
+            fd.write(new_str)
+
+def get_url(c, m):
+    return c[m.start():m.end()].replace(' ', '%20')
+
+quote()
